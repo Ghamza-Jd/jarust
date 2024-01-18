d2834038bf1cff0784c717821ca407277f0f2f09
Hamza Jadid <haljadid@proximie.com>
2024-01-18 14:41:45 +0200

    feat: created wasm ws transport

---
+++ jarust/src/transport/wasm_wss.rs
@@ -0,0 +1,24 @@
+use super::trans::Transport;
+use crate::prelude::*;
+use async_trait::async_trait;
+use tokio::sync::mpsc;
+
+pub struct WasmWsTransport;
+
+#[async_trait]
+impl Transport for WasmWsTransport {
+    fn new() -> Self {
+        Self
+    }
+
+    async fn connect(&mut self, uri: &str) -> JaResult<mpsc::Receiver<String>> {
+        log::error!("WASM support is WIP!");
+        let websocket = web_sys::WebSocket::new_with_str(uri, "janus-protocol").unwrap();
+        todo!("WASM support is WIP!")
+    }
+
+    async fn send(&mut self, _data: &[u8]) -> JaResult<()> {
+        log::error!("WASM support is WIP!");
+        todo!("WASM support is WIP!")
+    }
+}
