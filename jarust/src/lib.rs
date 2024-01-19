62f4c66d25495dc34ee844f53fb1e5c4b583a1dd
Hamza Jadid <haljadid@proximie.com>
2024-01-20 01:36:14 +0200

    chore: remove websys for now and added wasm connect function

--- jarust/src/lib.rs
+++ jarust/src/lib.rs
@@ -1,9 +1,3 @@
-use crate::transport::trans::Transport;
-use jaconfig::JaConfig;
-use jaconfig::TransportType;
-use jaconnection::JaConnection;
-use prelude::JaResult;
-
 pub mod error;
 pub mod jaconfig;
 pub mod jaconnection;
@@ -18,12 +12,25 @@ mod nsp_registry;
 mod tmanager;
 mod utils;
 
+use crate::transport::trans::Transport;
+use jaconfig::JaConfig;
+use jaconfig::TransportType;
+use jaconnection::JaConnection;
+use prelude::JaResult;
+
 #[cfg(not(target_family = "wasm"))]
 /// Creates a new connection with janus server from the provided configs
 pub async fn connect(jaconfig: JaConfig, transport_type: TransportType) -> JaResult<JaConnection> {
     let transport = match transport_type {
         jaconfig::TransportType::Wss => transport::wss::WebsocketTransport::new(),
     };
+    connect_with_transport(jaconfig, transport).await
+}
+
+#[cfg(target_family = "wasm")]
+/// Creates a new connection with janus server from the provided configs
+pub async fn connect(jaconfig: JaConfig, transport_type: TransportType) -> JaResult<JaConnection> {
+    let transport = transport::wasm_wss::WasmWsTransport;
     connect_with_transport(jaconfig, transport).await
 }
 
