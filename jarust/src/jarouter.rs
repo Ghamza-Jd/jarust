7fe963bea59c01c31954bc8b4c1cba90f2f97694
Hamza Jadid <haljadid@proximie.com>
2024-02-01 00:14:06 +0200

    feat: removed utils and moved getting path to jarouter

--- jarust/src/jarouter.rs
+++ jarust/src/jarouter.rs
@@ -1,6 +1,7 @@
 use crate::jaconfig::CHANNEL_BUFFER_SIZE;
 use crate::japrotocol::JaResponse;
 use crate::prelude::*;
+use serde_json::Value;
 use std::collections::HashMap;
 use std::ops::Deref;
 use std::ops::DerefMut;
@@ -108,6 +109,29 @@ impl JaRouter {
 impl JaRouter {
     pub fn root_path(&self) -> String {
         self.shared.root_path.clone()
+    }
+
+    pub fn path_from_request(request: &Value) -> Option<String> {
+        if let (Some(session_id), Some(handle_id)) = (
+            request["session_id"].as_u64(),
+            request["handle_id"].as_u64(),
+        ) {
+            Some(format!("{session_id}/{handle_id}"))
+        } else {
+            request["session_id"]
+                .as_u64()
+                .map(|session_id| format!("{session_id}"))
+        }
+    }
+
+    pub fn path_from_response(response: JaResponse) -> Option<String> {
+        let Some(session_id) = response.session_id else {
+            return None;
+        };
+        let Some(sender) = response.sender else {
+            return Some(format!("{session_id}"));
+        };
+        Some(format!("{session_id}/{sender}"))
     }
 }
 
