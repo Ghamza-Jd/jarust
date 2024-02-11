8652afa36292f4b5d98bcf07bca30eb17148907a
Hamza Jadid <haljadid@proximie.com>
2024-02-11 21:16:49 +0200

    feat: make tokio-rt a feature flag

---
+++ jarust/src/jatask/mod.rs
@@ -0,0 +1,14 @@
+#[cfg(feature = "tokio-rt")]
+#[path = "tokio_rt.rs"]
+pub mod jatask_rt;
+
+use futures_util::Future;
+pub use jatask_rt::AbortHandle;
+
+pub fn spawn<F>(future: F) -> AbortHandle
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    jatask_rt::spawn(future)
+}
