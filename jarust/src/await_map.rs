c6ac686e30e3b3ea4fb5998ba65afbf9c112400a
Hamza Jadid <haljadid@proximie.com>
2024-03-28 05:03:15 +0200

    feat: added tracing on await map

--- jarust/src/await_map.rs
+++ jarust/src/await_map.rs
@@ -1,4 +1,5 @@
 use dashmap::DashMap;
+use std::fmt::Debug;
 use std::hash::Hash;
 use std::sync::Arc;
 use tokio::sync::Mutex as AsyncMutex;
@@ -6,8 +7,8 @@ use tokio::sync::Notify;
 
 pub struct AwaitMap<K, V>
 where
-    K: Eq + Hash + Clone,
-    V: Clone,
+    K: Eq + Hash + Clone + Debug,
+    V: Clone + Debug,
 {
     map: Arc<DashMap<K, V>>,
     notifiers: AsyncMutex<DashMap<K, Arc<Notify>>>,
@@ -15,8 +16,8 @@ where
 
 impl<K, V> AwaitMap<K, V>
 where
-    K: Eq + Hash + Clone,
-    V: Clone,
+    K: Eq + Hash + Clone + Debug,
+    V: Clone + Debug,
 {
     pub fn new() -> Self {
         Self {
@@ -25,15 +26,21 @@ where
         }
     }
 
+    #[tracing::instrument(level = tracing::Level::TRACE, skip(self))]
     pub async fn insert(&self, k: K, v: V) {
+        tracing::debug!("Insert");
         self.map.insert(k.clone(), v);
         if let Some(notify) = self.notifiers.lock().await.remove(&k) {
             notify.1.notify_one();
+            tracing::debug!("Notify a sleeping task");
         }
     }
 
+    #[tracing::instrument(level = tracing::Level::TRACE, skip(self))]
     pub async fn get(&self, k: K) -> Option<V> {
+        tracing::debug!("Get");
         if self.map.contains_key(&k) {
+            tracing::debug!("Contains key");
             return self.map.get(&k).map(|entry| entry.value().clone());
         }
 
@@ -43,15 +50,17 @@ where
         notifiers.entry(k.clone()).or_insert(notify.clone());
         drop(notifiers);
 
+        tracing::debug!("Sleep until notified");
         notify.notified().await;
+        tracing::debug!("Notified");
         self.map.get(&k).map(|entry| entry.value().clone())
     }
 }
 
 impl<K, V> Default for AwaitMap<K, V>
 where
-    K: Eq + Hash + Clone,
-    V: Clone,
+    K: Eq + Hash + Clone + Debug,
+    V: Clone + Debug,
 {
     fn default() -> Self {
         Self::new()
