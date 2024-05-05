0a127dcd895c46c58290db1f9fe901a65099b79b
Hamza Jadid <haljadid@proximie.com>
2024-05-05 18:14:41 +0300

    feat(echotest): added generic events coming from jahandle

--- jarust_plugins/src/echotest/events.rs
+++ jarust_plugins/src/echotest/events.rs
@@ -1,7 +1,29 @@
+use jarust::error::JaError;
+use jarust::japrotocol::GenericEvent;
+use jarust::japrotocol::JaHandleEvent;
 use serde::Deserialize;
 
+pub enum Events {
+    PluginEvent(PluginEvent),
+    GenericEvent(GenericEvent),
+}
+
 #[derive(Debug, Deserialize)]
-pub enum EchoTestPluginEvent {
+pub enum PluginEvent {
     #[serde(untagged)]
     Result { echotest: String, result: String },
+}
+
+impl TryFrom<JaHandleEvent> for Events {
+    type Error = JaError;
+
+    fn try_from(value: JaHandleEvent) -> Result<Self, Self::Error> {
+        let event = match value {
+            JaHandleEvent::PluginEvent { plugin_data } => {
+                Self::PluginEvent(serde_json::from_value::<PluginEvent>(plugin_data.data)?)
+            }
+            JaHandleEvent::GenericEvent(event) => Self::GenericEvent(event),
+        };
+        Ok(event)
+    }
 }
