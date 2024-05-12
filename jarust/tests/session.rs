09ae4028bfa68513c10787bfad556a063e8e3edb
Hamza Jadid <haljadid@proximie.com>
2024-05-13 00:32:12 +0300

    test: added session integration tests

---
+++ jarust/tests/session.rs
@@ -0,0 +1,96 @@
+mod mocks;
+
+use crate::mocks::mock_transport::MockTransport;
+use jarust::jaconfig::JaConfig;
+use jarust::japlugin::Attach;
+use jarust::japrotocol::ErrorResponse;
+use jarust::japrotocol::JaData;
+use jarust::japrotocol::JaResponse;
+use jarust::japrotocol::JaSuccessProtocol;
+use jarust::japrotocol::ResponseType;
+
+#[tokio::test]
+async fn it_successfully_attach_to_handle() {
+    let config = JaConfig::builder()
+        .url("mock://some.janus.com")
+        .namespace("mock")
+        .build();
+    let (transport, server) = MockTransport::transport_server_pair();
+
+    let creation_msg = serde_json::to_string(&JaResponse {
+        janus: ResponseType::Success(JaSuccessProtocol::Data {
+            data: JaData { id: 2 },
+        }),
+        transaction: None,
+        session_id: None,
+        sender: None,
+        establishment_protocol: None,
+    })
+    .unwrap();
+
+    let mut connection = jarust::connect_with_transport(config, transport)
+        .await
+        .unwrap();
+
+    server.mock_send_to_client(&creation_msg).await;
+    let session = connection.create(10).await.unwrap();
+
+    let attachment_msg = serde_json::to_string(&JaResponse {
+        janus: ResponseType::Success(JaSuccessProtocol::Data {
+            data: JaData { id: 3 },
+        }),
+        transaction: None,
+        session_id: Some(2),
+        sender: None,
+        establishment_protocol: None,
+    })
+    .unwrap();
+    server.mock_send_to_client(&attachment_msg).await;
+    let result = session.attach("mock.plugin.test").await;
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn it_fails_to_attach_session() {
+    let config = JaConfig::builder()
+        .url("mock://some.janus.com")
+        .namespace("mock")
+        .build();
+    let (transport, server) = MockTransport::transport_server_pair();
+
+    let creation_msg = serde_json::to_string(&JaResponse {
+        janus: ResponseType::Success(JaSuccessProtocol::Data {
+            data: JaData { id: 2 },
+        }),
+        transaction: None,
+        session_id: None,
+        sender: None,
+        establishment_protocol: None,
+    })
+    .unwrap();
+
+    let mut connection = jarust::connect_with_transport(config, transport)
+        .await
+        .unwrap();
+
+    server.mock_send_to_client(&creation_msg).await;
+    let session = connection.create(10).await.unwrap();
+
+    let error = serde_json::to_string(&JaResponse {
+        janus: ResponseType::Error {
+            error: ErrorResponse {
+                code: 0,
+                reason: "".to_string(),
+            },
+        },
+        transaction: None,
+        session_id: Some(2),
+        sender: None,
+        establishment_protocol: None,
+    })
+    .unwrap();
+
+    server.mock_send_to_client(&error).await;
+    let result = session.attach("mock.plugin.test").await;
+    assert!(result.is_err());
+}
