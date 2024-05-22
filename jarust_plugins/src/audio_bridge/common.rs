dc0236e1b6f59becf0e564ad514e20e7d6c4420a
Hamza Jadid <haljadid@proximie.com>
2024-05-22 08:31:03 +0300

    docs: added identifier clarification

--- jarust_plugins/src/audio_bridge/common.rs
+++ jarust_plugins/src/audio_bridge/common.rs
@@ -1,9 +1,14 @@
 use serde::Deserialize;
 use serde::Serialize;
 
+/// Rooms and Participants Identifier.
+///
+/// Identifier should be by default unsigned integer, unless configured otherwise in the audiobridge config.
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 #[serde(untagged)]
 pub enum Identifier {
+    /// String Identifier
     String(String),
+    /// Unsigned Integer Identifier
     Uint(u64),
 }
