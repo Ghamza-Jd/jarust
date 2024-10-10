b93e3fb22040f15dbb336fa84476cbc092392440
Matthieu Rogez <matthieu.rogez@fivesgroup.com>
2024-10-10 09:35:49 +0000

    feat: add partial support for streaming info request
    only live RTP streaming source are supported.

--- jarust_plugins/src/streaming/responses.rs
+++ jarust_plugins/src/streaming/responses.rs
@@ -68,3 +68,71 @@ pub struct RtpMediaListed {
     pub msid: Option<String>,
     pub age_ms: Option<u64>,
 }
+
+// https://github.com/meetecho/janus-gateway/blob/v1.2.4/src/plugins/janus_streaming.c#L3172-L3309
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
+pub struct MountpointInfoRsp {
+    pub info: MountpointInfo,
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
+pub struct MountpointInfo {
+    pub id: JanusId,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub metadata: Option<String>,
+    pub secret: Option<String>,
+    pub pin: Option<String>,
+    #[serde(default)]
+    pub is_private: bool,
+    pub enabled: bool,
+    pub viewers: Option<u64>,
+    /// <live|on demand>
+    #[serde(rename = "type")]
+    pub mountpoint_type: String,
+    // TODO: add support for non RTP live mountpoints
+    #[serde(default)]
+    pub rtsp: bool,
+    pub url: Option<String>,
+    pub rtsp_user: Option<String>,
+    pub rtsp_pwd: Option<String>,
+    pub rtsp_quirk: Option<bool>,
+    #[serde(default)]
+    pub srtp: bool,
+    #[serde(default)]
+    pub collision: i32,
+    #[serde(default)]
+    pub threads: i32,
+    pub host: Option<String>,
+    pub media: Vec<RtpMediaInfo>,
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
+pub struct RtpMediaInfo {
+    pub mindex: u64,
+    #[serde(rename = "type")]
+    pub media_type: String,
+    pub mid: String,
+    pub label: String,
+    pub msid: Option<String>,
+    pub pt: Option<u8>, // 0-127
+    pub codec: Option<String>,
+    pub rtpmap: Option<String>,
+    pub fmtp: Option<String>,
+    #[serde(default)]
+    pub videobufferkf: bool,
+    #[serde(default)]
+    pub videosimulcast: bool,
+    #[serde(default)]
+    pub videosvc: bool,
+    #[serde(default)]
+    pub skew_compensation: bool,
+    pub port: Option<u16>,
+    pub rtcpport: Option<u16>,
+    pub port2: Option<u16>,
+    pub port3: Option<u16>,
+    // <text|binary>
+    pub datatype: Option<String>,
+    pub age_ms: Option<u64>,
+    pub recording: Option<String>,
+}
