7a16e45cd86882b720c9b5f5680762c9a18b4f47
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-10-11 21:11:21 +0300

    docs: added docs for config fields

--- jarust/src/jaconfig.rs
+++ jarust/src/jaconfig.rs
@@ -1,8 +1,13 @@
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub struct JaConfig {
+    /// Url to janus server
     pub url: String,
+    /// Janus api secret if any
     pub apisecret: Option<String>,
+    /// root path for janus, when using HTTP it should be `janus` unless it was changed
+    /// in janus config
     pub server_root: String,
+    /// Ring buffer capacity, used when picking WebSocket janus api
     pub capacity: usize,
 }
 
