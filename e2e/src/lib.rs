66a64bfdd2a1509e8834cfe61adba0f1f5243e49
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-11-02 21:50:47 +0200

    fix: made janus id uint as u32, janus doesn't support u64 - thanks e2es

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,1 +1,11 @@
-// Empty, this is a test crate
+use tracing_subscriber::EnvFilter;
+
+/// For debuggin e2e tests
+pub fn init_tracing_subscriber() {
+    let env_filter = EnvFilter::from_default_env()
+        .add_directive("jarust=trace".parse().unwrap())
+        .add_directive("jarust_plugins=trace".parse().unwrap())
+        .add_directive("jarust_interface=trace".parse().unwrap())
+        .add_directive("jarust_rt=trace".parse().unwrap());
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+}
